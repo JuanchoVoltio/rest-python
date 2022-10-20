9ea182a25734e5505382cec194549793dbe23f8e
Henry L. Gómez Ortiz <henryl.gomez@gmail.com>
2022-10-19 20:56:25 -0500

    Agregar ejemplo 03

---
+++ code/03/main.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    nombre: str
+    descripción: Union[str, None] = None
+    precio_base: float
+    descuento: Union[float, None] = None
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    precio_final = item.precio_base * (1 - item.descuento/100)
+    return {"item": item, "precio": precio_final}
