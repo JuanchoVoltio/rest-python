2cf621364e1f62c9079bd71aa706f13d8093b65c
Henry L. Gómez Ortiz <henryl.gomez@gmail.com>
2022-10-19 20:33:53 -0500

    Agregar nueva práctica y comentarios

--- code/01/main.py
+++ code/01/main.py
@@ -7,6 +7,11 @@ from typing import Optional
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/') #Definición del endpoint. La anotación indica que este soportará un método GET
 def saludar():
     return "Hola Mundo"
+
+@app.get('?') #TODO: Práctica 2: Definir el path del endpoint
+def sumar():
+    #TODO: Práctica 2: Implementar la lógica requerida
+    pass
